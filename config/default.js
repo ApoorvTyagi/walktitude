14c2d1bcbd19185a29caf23b56cecc777c56e47b
Apoorv Tyagi <tyagi.apoorv133@gmail.com>
2024-04-27 19:38:57 +0530

    Updated DB

--- config/default.js
+++ config/default.js
@@ -2,12 +2,12 @@ const config = {
   APP: {
     NODE_ENV: "development",
     PORT: 3000,
-    JWT_PUBLIC_KEY: '',
-    JWT_PRIVATE_KEY: '',
+    JWT_PUBLIC_KEY: "",
+    JWT_PRIVATE_KEY: "",
     NEAREST_WALKER_MAX_DISTANCE_FOR_SEARCH: 500,
   },
   DB: {
-    URI: "mongodb://localhost:27017/walktitude",
+    URI: "mongodb+srv://admin:admin@cluster0.ordawt9.mongodb.net/walktitude?retryWrites=true&w=majority",
   },
 };
 
